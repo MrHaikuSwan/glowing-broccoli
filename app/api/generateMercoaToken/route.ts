362530bbc7ecd54689dfd6a902abcc3adf8f6ddf
ashwinak <ashwinak@umich.edu>
2024-06-06 11:51:43 -0400

    Added permissions to JWT options

--- app/api/generateMercoaToken/route.ts
+++ app/api/generateMercoaToken/route.ts
@@ -11,16 +11,16 @@ export async function GET(request: NextRequest) {
   const entId = request.nextUrl.searchParams.get("entId")!;
   const userId = request.nextUrl.searchParams.get("userId");
 
+  const options = {
+    expiresIn: "1h",
+    entity: { enableMercoaPayments: true },
+    pages: { paymentMethods: true, representatives: true, notifications: true },
+  };
+
   const token =
     userId === undefined
-      ? await mercoa.entity.getToken(entId, {
-          expiresIn: "1h",
-          pages: { paymentMethods: true },
-        })
-      : await mercoa.entity.user.getToken(entId, userId!, {
-          expiresIn: "1h",
-          pages: { paymentMethods: true },
-        });
+      ? await mercoa.entity.getToken(entId, options)
+      : await mercoa.entity.user.getToken(entId, userId!, options);
 
   return Response.json({
     token,
