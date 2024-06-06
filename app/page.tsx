65ec6ac4a1369d977602b7e24ae1253839d9f973
ashwinak <ashwinak@umich.edu>
2024-06-06 04:16:10 -0400

    Reformatted login page for user accounts

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/login-form";
 
 export default function LoginPage() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-800">
       <LoginForm />
     </div>
   );
--- components/login-form.tsx
+++ components/login-form.tsx
@@ -5,12 +5,14 @@ import Link from "next/link";
 import {
   Card,
   CardContent,
-  CardFooter,
   CardHeader,
   CardTitle,
+  CardDescription,
 } from "@/components/ui/card";
 
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 import { authenticate } from "@/lib/actions";
 import { useFormState, useFormStatus } from "react-dom";
 
@@ -19,38 +21,56 @@ export function LoginForm() {
 
   // TODO: Should add an animation for login button
   return (
-    <div className="w-full max-w-md">
+    <div className="w-full max-w-sm">
       <form action={dispatch}>
-        <Card>
-          <CardHeader className="space-y-1 text-center">
-            <CardTitle className="text-3xl font-bold">
-              Business Sign In
-            </CardTitle>
+        <Card className="mx-auto max-w-sm">
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl font-bold">Login</CardTitle>
+            <CardDescription>
+              Enter your Company ID, username and password to login to your
+              account
+            </CardDescription>
           </CardHeader>
           <div className="text-center text-red-400 pb-4">
             {errorMessage && <p>{errorMessage}</p>}
           </div>
-          <CardContent className="space-y-4">
-            <div className="space-y-2">
-              <Input
-                id="identifier"
-                name="identifier"
-                type="text"
-                placeholder="Company ID"
-              />
-            </div>
-            <div className="space-y-2">
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                placeholder="Password"
-              />
+          <CardContent>
+            <div className="space-y-4">
+              <div className="space-y-2">
+                <Label htmlFor="identifier">Company ID</Label>
+                <Input
+                  id="identifier"
+                  name="identifier"
+                  type="text"
+                  placeholder="acmecorp"
+                  required
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="userid">User ID</Label>
+                <Input
+                  id="userid"
+                  name="userid"
+                  type="text"
+                  placeholder="johndoe"
+                  required
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="1234"
+                  required
+                />
+              </div>
+              <div className="pt-8">
+                <LoginButton />
+              </div>
             </div>
           </CardContent>
-          <CardFooter className="flex flex-col">
-            <LoginButton />
-          </CardFooter>
         </Card>
       </form>
     </div>
@@ -61,7 +81,7 @@ function LoginButton() {
   const { pending } = useFormStatus();
 
   return (
-    <button
+    <Button
       aria-disabled={pending}
       type="submit"
       onClick={(event) => {
@@ -71,7 +91,7 @@ function LoginButton() {
       }}
       className="w-full"
     >
-      Sign In
-    </button>
+      Login
+    </Button>
   );
 }
